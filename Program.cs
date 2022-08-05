ef974ced9311c8139e90c635f8cfbe3ca9a6a3fa
Alex <belova.al.ig@gmail.com>
2022-08-05 21:32:01 +0300

    First programm done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая на вход принимает число и выдаёт его 
+// // квадрат (число умноженное на само себя).
+
+// Console.Write("Insert a number: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = Convert.ToInt32(Math.Pow(number, 2));
+
+// Console.WriteLine("Square of your number is: " + sqrt);
+
+
+//2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
